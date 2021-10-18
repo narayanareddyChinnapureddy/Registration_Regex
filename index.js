243fd5517a8029e63a5d54c04aa7adc7dcc5bdab
Harshada Samant <harshada.samant@rehau.com>
2021-10-18 09:12:08 +0530

    Initial load

---
+++ index.js
@@ -0,0 +1,4 @@
+var form = document.getElementById('form');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+var span = document.getElementsByTagName('span');
